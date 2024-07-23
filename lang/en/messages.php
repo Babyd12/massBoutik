f18bdc133021767400dd123ef19b119533ad182a
mcire <mamadcir24@gmail.com>
2024-07-23 02:21:39 +0100

    configuration of multilangage frensh and englis

---
+++ lang/en/messages.php
@@ -0,0 +1,153 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'These credentials do not match our records.',
+    'password' => 'The provided password is incorrect.',
+    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Customization Language Lines
+    |--------------------------------------------------------------------------
+    |*/
+    'Product' => 'Product',
+    'Products' => 'Products',
+    'Lougout' => 'Logout',
+    'Enable' => 'Enable',
+    'Disable' => 'Disable',
+
+    'Name' => 'Name',
+    'Day' => 'Day',
+    'Month' => 'Month',
+    'Year' => 'Year',
+    'Apply' => 'Apply',
+    'Select' => 'Select',
+
+    'A_female' => 'A',
+    'A_male' => 'A',
+    'The_female' => 'The',
+    'The_male' => 'The',
+
+
+    'Period' => 'Period',
+    'Dashboard' => 'Dashboard',
+    'Number' => 'Number',
+    'Amount' => 'Amount',
+    'Total' => 'Total',
+    'Sell' => 'Sell',
+    'Sells' => 'Sells',
+    'Price' => 'Price',
+
+
+    'Sales Amount' => 'Sales Amount',
+    'Number of sales' => 'Number of sales',
+    'Daily' => 'Daily',
+    'Monthly' => 'Monthly',
+    'Weekly' => 'Weekly',
+    'Yearly' => 'Yearly',
+    'Statistics' => 'Statistics',
+
+    'PurchasePrice' => 'Purchase Price',
+    'Sellingprice' => 'Selling Price',
+    'State' => 'State',
+    'Unit Per Pack' => 'Unit Per Pack',
+    'Unit Per Packs' => 'Units Per Pack',
+    'Unit Per Pack Id' => 'Unit Per Pack Id',
+    'Position' => 'Position',
+    'Rays' => 'Rays',
+    'Ray' => 'Ray',
+    'Title' => 'Title',
+
+
+    // Crud word
+    'Create' => 'Create',
+    'Update' => 'Update',
+    'Delete' => 'Delete',
+    'Create New' => 'Create New',
+    'created_at' => 'Created at',
+    'updated_at' => 'Updated at',
+    'Edit' => 'Edit',
+    'Back' => 'Back',
+    'Show' => 'Show',
+
+    //stock
+    'Stocks' => 'Stocks',
+    'Stock' => 'Stock',
+    'Choose' => 'Choose',
+    'Choose Product' => 'Choose Product',
+    'Select',
+    'Sell Price' => 'Sell Price',
+    'Buy Price' => 'Buy Price',
+    'Quantity' => 'Quantity',
+    'Operation' => 'Operation',
+    'Submit' => 'Submit',
+    'clearance' => 'Sell', //exception not start by capital letter
+    'storage' => 'Buy',  //exception not start by capital letter
+    'New Price' => 'New Price',
+    'Create Sell' => 'Create Sell',
+    'Add stock' => 'Add Stock',
+    'Select a product' => 'Select a Product',
+    'Choose a Product' => 'Choose a Product',
+
+    //user
+    'Full Name' => 'Full Name',
+    'Nick Name' => 'Nick Name',
+    'Description' => 'Description',
+    'Picture' => 'Picture',
+    'Email' => 'Email',
+    'Role' => 'Role',
+    'Phone' => 'Phone',
+
+    //unit per pack
+    'The purchase unit is empty, Please add one before add product' =>  'The purchase unit is empty, please add one before adding the product',
+
+    //lend 
+    'Quantity' => 'Quantity',
+    'User' => 'User',
+    'Date of lend' => 'Date of lend',
+    'Lends' => 'Lends',
+
+
+    //sidebar
+    'Home' => 'Home',
+    'Product management' => 'Product Management',
+    'Customers management' => 'Customer Management',
+    'Storage management' => 'Stock Management',
+    'Customers' => 'Customers',
+    'Customer' => 'Customer',
+    'Show rays' => 'Show Rays',
+    'Show products' => 'Show Products',
+    'Show sales' => 'Show Sales',
+    'Show customers' => 'Show Customers',
+    'Show Borrowers' => 'Show Borrowers',
+    'Borrowers' => 'Borrowers',
+    'Buys - Sells' => 'Buys - Sells',
+    'Buys and Sells' => 'Buys and Sells',
+    'Show unit per pack' => 'Show Units Per Pack',
+    'Show buys and sells' => 'Show Buys and Sells',
+    'Add buys or sells' => 'Add Buys or Sells',
+    'Add lend' => 'Add Lend',
+
+    //home 
+    'Expected profit' => 'Expected Profit',
+    'Finance' => 'Finance',
+    'Forecast' => 'Forecast',
+    'Total Profit Expected for All Products' => 'Total Profit Expected for All Products',
+
+
+    // error message 
+    'Quantity must be less or equal to current stock' => 'Quantity must be less than or equal to current stock',
+
+];
